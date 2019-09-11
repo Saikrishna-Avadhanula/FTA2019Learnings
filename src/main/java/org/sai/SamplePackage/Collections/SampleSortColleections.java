011199123d671eb07f1a09aa4661d0e59ca5fa88
savadhanula <avadhanulasai@gmail.com>
2019-09-11 11:56:21 +0530

    Update

--- src/main/java/org/sai/SamplePackage/Collections/SampleSortColleections.java
+++ src/main/java/org/sai/SamplePackage/Collections/SampleSortColleections.java
@@ -1,24 +1,76 @@
 package org.sai.SamplePackage.Collections;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class SampleSortColleections {
 
 
     public static void main( String[] aaargs){
-        List  list = new ArrayList();
+//        List  list = new ArrayList();
+//
+////        list.add(5);
+////        list.add(4);
+////        list.add(6);
+//        list.add("sai");
+//list.add("krishna");
+//        Collections.sort(list);
+//
+//        for(int i=0; i<list.size(); i++){
+//            System.out.println(list.get(i));
+//        }
+//
+//        List lList= new LinkedList();
+//        lList.add(234);
+//        lList.add(123);
+//        lList.add(234);
+////        lList.add('c');
+////        lList.add("String");
+//
+//        for (Object ob: lList
+//             ) {
+//
+//            System.out.println(ob);
+//        }
+//        Collections.sort(lList);
 
-        list.add(5);
-        list.add(4);
-        list.add(6);
+//        Iterator<Integer> integerIterator = lList.listIterator();
+//
+//            while(integerIterator.hasNext()){
+//                System.out.println(integerIterator.next());
+//        }
+//
+//            HashSet hs = new HashSet();
 
-        Collections.sort(list);
+        LinkedList alist = new LinkedList();
+        alist.add("Abc");
+        alist.add("Abc");
+        alist.add("abc");
+        alist.add("abc");
+            LinkedHashSet hs = new LinkedHashSet();
+            hs.add("Ravi");
+            hs.add("Tej");
+            hs.add("Sai");
+            hs.add("Ravi");
+            hs.add("ravi");
 
-        for(int i=0; i<list.size(); i++){
-            System.out.println(list.get(i));
-        }
+            HashSet lhs = new HashSet(alist);
+            Iterator i=lhs.iterator();
+            while(i.hasNext()){
+                System.out.println(i.next());
+            }
+        System.out.println(alist.contains("abc"));
+
+
+//
+//
+//
+//            Iterator ite = hs.iterator();
+//            while (ite.hasNext()){
+//                System.out.println(ite.next());
+//            }
+
+
+
 
 
 
