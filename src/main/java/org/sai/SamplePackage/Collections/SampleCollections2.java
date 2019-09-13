21db4fbe7f4ec20f0d8f1d70bd4105acdb0a05b7
savadhanula <avadhanulasai@gmail.com>
2019-09-13 11:56:28 +0530

    9/13 code...!!

---
+++ src/main/java/org/sai/SamplePackage/Collections/SampleCollections2.java
@@ -0,0 +1,84 @@
+package org.sai.SamplePackage.Collections;
+
+import java.util.*;
+
+public class SampleCollections2 {
+    /*
+    asynchronised
+    Duplicates allowed
+    ArrayList     >>>>> Dynamic array
+    LinkedList    >>>>>
+
+    Duplicates not allowed
+    LinkedHashSet >>>>> Insertion order no sorting is available
+    TreeSet       >>>>> stored in Ascending order, Generics should be given
+    HashSet       >>>>>
+
+     */
+    public static void main(String[] args){
+
+        ArrayList    arrayList      = new ArrayList();
+        LinkedList   linkedList     = new LinkedList();
+        LinkedHashSet linkedHashSet = new LinkedHashSet();
+        TreeSet     treeSet         = new TreeSet();
+        HashSet     hashSet         = new HashSet();
+
+        add(arrayList);
+        add(linkedList);
+        add(linkedHashSet);
+//        add(treeSet);
+        add(hashSet);
+
+        print(arrayList);
+        print(linkedList);
+        print(linkedHashSet);
+//        print(treeSet);
+        print(hashSet);
+
+    }
+    public static void add(List list){
+        list.add(1);
+        list.add(2);
+        list.add(5);
+        list.add(1);
+        list.add(4);
+        list.add(3);
+        list.add("SAI");
+        list.add("sai");
+        list.add("SaI");
+        list.add(5.5);
+
+    }
+    public static void add(Set list){
+        list.add(1);
+        list.add(2);
+        list.add(5);
+        list.add(1);
+        list.add(4);
+        list.add(3);
+        list.add("SAI");
+        list.add("sai");
+        list.add("SaI");
+        list.add(5.5);
+
+    }
+    public static void print(List list){
+        System.out.println("-------------"+list.toString()+"---------------");
+        System.out.println("-------------"+list.getClass().getSimpleName()+"---------------");
+        Iterator iterator = list.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+    public static void print(Set list){
+        System.out.println("-------------"+list.toString()+"---------------");
+        System.out.println("-------------"+list.getClass().getSimpleName()+"---------------");
+        Iterator iterator = list.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+
+}
---
+++ src/main/java/org/sai/SamplePackage/Collections/SampleMapCollection.java
@@ -0,0 +1,39 @@
+package org.sai.SamplePackage.Collections;
+
+import java.util.*;
+
+public class SampleMapCollection {
+    public static void main(String[] args){
+
+        Hashtable<Integer,String> hashtable = new Hashtable<Integer, String>();
+        HashMap<Integer,String>   hashMap   = new HashMap<Integer, String>();
+
+        hashtable.put(1,"Avadhanula");
+        hashtable.put(2,"Sai");
+        hashtable.put(3,"Krishna");
+
+        Set<Integer> set = hashtable.keySet();
+        Iterator<Integer> integerIterator = set.iterator();
+        while(integerIterator.hasNext()){
+            Integer key = integerIterator.next();
+            System.out.println(key+"---------"+hashtable.get(key));
+        }
+
+
+        hashtable.put(1,"Avadhanula_update");
+        hashtable.put(2,"Sai_update");
+        hashtable.put(3,"Krishna_update");
+
+        for (Map.Entry mapEntry: hashtable.entrySet()) {
+            System.out.println(mapEntry.getClass().getSimpleName()+"------->"+mapEntry.getKey()+"-----"+mapEntry.getValue());
+        }
+
+        hashMap.remove(1);
+
+
+
+    }
+
+
+
+}
